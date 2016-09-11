d2c53b96d422e5a6893461e6ab9fbfa8347f52c3
James Pegg <jamescpegg@gmail.com>
2016-09-11 22:34:10 +0100

    Fixed indenting in main app routes config

--- public/app/monoture.js
+++ public/app/monoture.js
@@ -3,24 +3,23 @@
 var monoture = angular.module('monoture', ['ngRoute', 'ngStorage']);
 
 monoture.config(function($routeProvider) {
-
-    $routeProvider.when('/', {
-      templateUrl : 'app/views/posts.html',
-      controller : 'PostListController'
-    }).
-    when('/login', {
-      templateUrl : 'app/views/login.html',
-      controller : 'LoginController'
-    }).
-    when('/edit/:post', {
-      templateUrl : 'app/views/edit.html',
-      controller : 'PostEditController'
-    }).
-    when('/new', {
-      templateUrl : 'app/views/edit.html',
-      controller : 'PostCreateController'      
-    }).
-    otherwise({
-        redirectTo: '/login'
-    });
+  $routeProvider.when('/', {
+    templateUrl : 'app/views/posts.html',
+    controller : 'PostListController'
+  }).
+  when('/login', {
+    templateUrl : 'app/views/login.html',
+    controller : 'LoginController'
+  }).
+  when('/edit/:post', {
+    templateUrl : 'app/views/edit.html',
+    controller : 'PostEditController'
+  }).
+  when('/new', {
+    templateUrl : 'app/views/edit.html',
+    controller : 'PostCreateController'
+  }).
+  otherwise({
+    redirectTo: '/login'
+  });
 });
